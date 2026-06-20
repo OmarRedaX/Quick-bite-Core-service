4e2c01c48c9316bdc9bd1abb2a196664da71b672
Omar Reda <redaomar1999@gmail.com>
2026-06-20 05:50:27 +0300

    fix: smal fix in import

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import { healthRouter } from "./app/health/health.routes";
 import { authRouter } from "./app/auth/routes";
-import { userRoutes } from "./app/user/routes";
-import { customerAddressRoutes } from "./app/address/routes";
+import { userRouter } from "./app/user/routes";
+import {customerAddressRouter} from "./app/customer-address/routes";
 
 export const routes = Router();
 
 routes.use("/health", healthRouter);
 
 //user
-routes.use("/user", userRoutes);
+routes.use("/user", userRouter);
 //auth
 routes.use("/auth", authRouter);
 // customer
-routes.use("/customer", customerAddressRoutes);
+routes.use("/customer/addresses", customerAddressRouter);
 //menu
 
 //restaurant
