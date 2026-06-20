7230d0f7ce170f43aa71c6a7f7a4c5872781dc5d
Omar Reda <redaomar1999@gmail.com>
2026-06-20 05:48:30 +0300

    refactor: update cookies fn

--- src/common/utils/cookie.utils.ts
+++ src/common/utils/cookie.utils.ts
@@ -1,25 +1,17 @@
-import { CookieOptions } from "express";
-import { toMilliseconds } from "./time.utils";
-import { env } from "../config/env";
+import {Response} from "express";
+import {env} from "../config/env";
+import {toMs} from "./time.utils";
 
-export const getCookieOptions = (type: "access" | "refresh"): CookieOptions => {
-
-    const baseOptions: CookieOptions = {
+export function setAuthCookies(res: Response, accessToken: string, refreshToken: string) {
+    res.cookie("access_token", accessToken, {
         httpOnly: true,
-        secure: env.nodeEnv === "production"
-    };
-
-    if (type === 'access') {
-        return {
-            ...baseOptions,
-             maxAge: toMilliseconds({ hours: 1 }),
-        };
-    }
-
-    // Refresh Token
-    return {
-        ...baseOptions,
-        maxAge: toMilliseconds({ days: 7 }),
-        path: '/api/auth/refresh-token',
-    };
+        secure: env.isProduction,
+        maxAge: toMs(1, 'h'),
+    });
+    res.cookie("refresh_token", refreshToken, {
+        httpOnly: true,
+        secure: env.isProduction,
+        maxAge: toMs(7, 'd'),
+        path: '/api/auth/refresh',
+    });
 }
