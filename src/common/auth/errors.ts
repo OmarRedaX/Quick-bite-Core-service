425c5d28cbd1519d301f7b9a2e143b640bbe009f
Omar Reda <redaomar1999@gmail.com>
2026-06-28 07:18:59 +0300

    feat: add unauthorized error

--- src/common/auth/errors.ts
+++ src/common/auth/errors.ts
@@ -2,3 +2,4 @@ import { AppError } from "../error/AppError";
 
 
 export const NotAuthenticated = new AppError("User not authenticated", 403);
+export const UnauthorizedError = new AppError("User not authorized", 403);
