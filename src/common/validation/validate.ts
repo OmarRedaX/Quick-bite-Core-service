a47d3c12316dddaf4cc2f53afb5862902dd1be81
Omar Reda <redaomar1999@gmail.com>
2026-06-28 07:20:06 +0300

    refactor: use plainToInstance in validation

--- src/common/validation/validate.ts
+++ src/common/validation/validate.ts
@@ -1,8 +1,9 @@
 import { validate } from "class-validator"
 import { AppError } from "../error/AppError";
+import { plainToInstance } from "class-transformer";
 
 export async function validateBody <T extends Object>(cls: new () => T, body: unknown) : Promise<T> {
-    const instance = Object.assign(new cls(), body)
+    const instance = plainToInstance(cls, body)
     const errors = await validate(instance, {whitelist: true})
 
     if (errors.length > 0) {
